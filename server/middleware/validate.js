b5a320f3e4b04d96967514f81df5c33fb88ba25e
unknown <muche>
2018-06-14 01:34:37 +0100

    add user validation

--- server/middleware/validate.js
+++ server/middleware/validate.js
@@ -29,6 +29,94 @@ class Validate {
   }
   /**
    *
+   * @static
+   *
+   * @param {object} req
+   *
+   * @param {object} res
+   *
+   * @param {function} next
+   *
+   * @returns {object} - JSON object and status code
+   *
+   * @memberof Validate
+   */
+  static signUp(req, res, next) {
+    const {
+      username,
+      firstName,
+      lastName,
+      email,
+      password
+
+    } = req.body;
+
+    const userData = {
+      username,
+      firstName,
+      lastName,
+      email,
+      password
+    };
+
+    const userDataRules = {
+      username: 'required|string|min:5',
+      firstName: 'required|string|alpha|min:2',
+      lastName: 'required|string|alpha|min:2',
+      email: 'required|string|email',
+      password: 'required|min:6'
+    };
+
+    const validation = new Validator(userData, userDataRules);
+    if (validation.passes()) {
+      next();
+    } else {
+      const errors = validation.errors.all();
+      return res.status(400)
+        .json({ message: errors });
+    }
+  }
+  /**
+   *
+   * @static
+   * @param {object} req
+   *
+   * @param {object} res
+   *
+   * @param {function} next
+   *
+   * @returns {object} - JSON object and status code
+   *
+   * @memberof Validate
+   */
+  static signIn(req, res, next) {
+    const {
+      username,
+      email,
+      password
+    } = req.body;
+
+    const userData = {
+      username: (username || email),
+      password
+    };
+
+    const userDataRules = {
+      username: 'required|string',
+      password: 'required|min:6',
+    };
+
+    const validation = new Validator(userData, userDataRules);
+    if (validation.passes()) {
+      next();
+    } else {
+      const errors = validation.errors.add();
+      return res.status(400)
+        .json({ message: errors });
+    }
+  }
+  /**
+   *
    * @param {request} req
    *
    * @param {res} res
