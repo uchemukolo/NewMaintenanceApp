15ce22b32e65931d438812da30f63d50b4227b25
unknown <muche>
2018-07-02 20:36:16 +0100

    validate admin controllers

--- server/middleware/validate.js
+++ server/middleware/validate.js
@@ -1,7 +1,6 @@
 import Validator from 'validatorjs';
+import db from '../models/index';
 /**
- *
- *
  * @class Validate
  */
 class Validate {
@@ -129,6 +128,147 @@ class Validate {
       return res.status(400)
         .send({ message: errors });
     }
+  }
+  /**
+   *
+   * @description
+   *
+   * @param {object} req
+   *
+   * @param {object} res
+   *
+   * @param {function} next
+   *
+   * @returns {object} - JSON object and status code.
+   *
+   * @memberof Validate
+   */
+  static checkRequestStatus(req, res, next) {
+    const { requestId } = req.params;
+
+    const query = {
+      text: 'SELECT * FROM requests WHERE id = $1 AND currentStatus = $2',
+      values: [requestId, 'Pending'],
+    };
+    db.query(query)
+      .then((result) => {
+        if (result.rows.length === 0 && req.decoded.role === 'Admin') {
+          return res.status(405).send({
+            message: 'The status of this request has been updated, you cannot approve or disapprove again, please check the Current status',
+          });
+        }
+        if (result.rows.length === 0) {
+          return res.status(405).send({
+            message: 'Admin has already looked into this request, Please check the current status of the request',
+            status: 'fail',
+          });
+        }
+        return next();
+      });
+  }
+  /**
+   *
+   * @static
+   * @param {object} req
+   *
+   * @param {object} res
+   *
+   * @param {function} next
+   *
+   * @returns {object} - JSON object and status code
+   *
+   * @memberof Validate
+   */
+  static checkApproved(req, res, next) {
+    const { requestId } = req.params;
+    const query = {
+      text: 'SELECT * FROM requests WHERE id = $1 AND currentStatus = $2',
+      values: [requestId, 'Approved'],
+    };
+    db.query(query)
+      .then((result) => {
+        if (result.rows.length === 0) {
+          return res.status(405).send({
+            message: 'This request has not been approved, Please check the current status of the request',
+          });
+        }
+
+        return next();
+      });
+  }
+  /**
+   *
+   * @static
+   * @param {object} req
+   *
+   * @param {object} res
+   *
+   * @param {function} next
+   *
+   * @returns {object} - JSON object and status code
+   *
+   * @memberof Validate
+   */
+  static resolvedRequest(req, res, next) {
+    const { requestId } = req.params;
+
+    const query = {
+      text: 'SELECT * FROM requests WHERE id = $1',
+      values: [requestId]
+    };
+
+    db.query(query)
+      .then((request) => {
+        if (request.rows[0].status === 'Resolved') {
+          return res.status(400).send({
+            message: 'Request has already been resolved, it can not be approved',
+          });
+        }
+        return next();
+      })
+      .catch((err) => {
+        res.status(400).send({
+          message: 'Resolved request cannot be modified',
+          error: err.message
+        });
+      });
+  }
+  /**
+   *
+   * @static
+   * @param {object} req
+   *
+   * @param {object} res
+   *
+   * @param {function} next
+   *
+   * @returns {object} - JSON object and status code
+   *
+   * @memberof Validate
+   */
+  static resolveCheck(req, res, next) {
+    const { requestId } = req.params;
+
+    const query = {
+      text: 'SELECT * FROM requests WHERE requestid = $1',
+      values: [requestId]
+    };
+
+    db.query(query)
+      .then((request) => {
+        if (request.rows[0].status === 'Disapproved' || request.rows[0].status === 'Pending') {
+          return res.status(400).send({
+            message: 'Only an approved request can be Resolved',
+          });
+        }
+        return next();
+      })
+      .catch((err) => {
+        res.status(400).send({
+          message: 'Could not retrieve request',
+          error: err.message
+        });
+      });
   }
 }
 
